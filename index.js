4d07ba156f651af857e618c96241b893bd1a0201
Kingsle <kingsley.onah@heirsinsurancegroup.com>
2026-08-13 10:31:04 +0100

    disable right click and devtools shortcuts

--- index.js
+++ index.js
@@ -199,3 +199,14 @@ function stopPlay() {
     activeButton.innerText = "stopped"
 
 }
+
+document.addEventListener('contextmenu', e => e.preventDefault());
+
+document.addEventListener('keydown', e => {
+  if (
+    e.key === 'F12' ||
+    (e.ctrlKey && e.shiftKey && ['I', 'J', 'C'].includes(e.key.toUpperCase()))
+  ) {
+    e.preventDefault();
+  }
+});
